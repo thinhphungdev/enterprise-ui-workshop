22675c7fd39fdf7e3a9c1391245447d6cdb43090
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-17 17:14:44 +0700

    feat: init vitest & interacting with the DOM

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,19 @@
-import { screen, render } from '@testing-library/react';
+// @vitest-environment happy-dom
+
+import { screen, render, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should increment when the "Increment" button is pressed', async () => {
+  render(<Counter />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount.textContent).toBe('0');
+
+  const incrementBtn = screen.getByRole('button', { name: 'Increment' });
+  fireEvent.click(incrementBtn);
+  expect(currentCount.textContent).toBe('1');
+});
