3020f6b2213608ff27fa513d4fd34f00b1813f3f
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-20 22:37:36 +0700

    mocking time

--- src/examples/time-zone/time-zone.test.tsx
+++ src/examples/time-zone/time-zone.test.tsx
@@ -1,12 +1,21 @@
-import { test, expect, vi } from 'vitest';
+import { test, expect, vi, beforeEach, afterEach } from 'vitest';
 import { render } from 'test/utilities';
 import TimeZone from '.';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+  vi.setSystemTime(1679492355195);
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
 
 test('it should render successfully', () => {
   render(<TimeZone />);
 });
 
-test.fails('should match the snapshot', async () => {
+test('should match the snapshot', async () => {
   const { container } = render(<TimeZone />);
   expect(container).toMatchSnapshot();
 });
